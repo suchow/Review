3389208cd4ae0ad97fd9ac71bde0d683247a4ba0
Jordan Suchow <suchow@fas.harvard.edu>
2012-12-15 15:55:39 -0500

    reactive templates instead of changeview

--- Review.js
+++ Review.js
@@ -1,28 +1,77 @@
 Figures = new Meteor.Collection("figures");
+Reviews = new Meteor.Collection("reviews");
 
 if (Meteor.isClient) {
   
+  Session.set('isreviewing', false);
+  Session.set('issubmitting', false);
+  Session.set('isbeingwelcomed', true);
+  
+  Template.writereview.isreviewing = function () {
+    return Session.get('isreviewing');
+  };
+    
   Template.writereview.events({
     'click #write-review-submit-button' : function () {
       changeview('write-review', 'welcome');
       var id = Session.get('fig_id_to_review');
       console.log(id);
       Figures.update(id, {
-        $push : { reviews: document.getElementById("write-review-description").value }
+      
+      // create a new review record
+      var id = Reviews.insert({
+      submission_time: Date.now(),
+              creator: -1,
+           figure_url: Session.get('fig_id_to_review'),
+                 text: document.getElementById("write-review-description").value,
+              ratings: new Array() 
       });
+      
+      // update figure record
+      Figures.update(Session.get('fig_id_to_review'), {
+        $push : { reviews: id }
+      });
+      
+      Session.set('isreviewing', false);
+      Session.set('isbeingwelcomed', true);
     }
   });
+  
+  Template.writereview.rendered = function () {
+    console.log('The template writereview was just rendered.');
+  };
+  
+  Template.writereview.figuretoreview = function () {
+    var fig = Figures.findOne({}, {sort : {good_reviews:1, submission_time:1}});
+    return fig.figure_url;
+  };
+
+  Template.welcome.newuser = function () {
+    if(Meteor.user() === null)
+      return true;
+    else
+      return false;
+  };
+  
+  Template.welcome.isbeingwelcomed = function () {
+    return Session.get('isbeingwelcomed');
+  };
 
   Template.welcome.events({
     'click #welcome-write-review' : function () {
-      showfiguretoreview();
-      changeview('welcome', 'write-review');
+      Session.set('isbeingwelcomed', false);
+      Session.set('isreviewing', true);
     },
     
     'click #welcome-get-review' : function () {
-      changeview('welcome', 'get-review')
+      Session.set('isbeingwelcomed', false);
+      Session.set('issubmitting', true);
     }
   });
+  
+  Template.getreview.issubmitting = function () {
+    return Session.get('issubmitting');
+  };
   
   Template.getreview.events({
     'change #get-review-upload-fp': function(evt) {
@@ -30,21 +79,24 @@ if (Meteor.isClient) {
       // show the newly uploaded figure
       var img = document.getElementById('get-review-upload-preview');
       img.src = Session.get('figure_url');
-      changeview('get-review-upload-fp-wrapper', 'get-review-upload-preview');
+      document.getElementById('get-review-upload-fp-wrapper').style.display = "none";
+      document.getElementById('get-review-upload-preview').style.display = "block";
     },
     
     'click #get-review-submit-button' : function () {      
       // create a new figure
       var id = Figures.insert({
       submission_time: Date.now(),
+              creator: -1,
            figure_url: Session.get('figure_url'),
                fields: document.getElementById("get-review-field").value,
           description: document.getElementById("get-review-description").value,
-              reviews: Array(), // list of review _id's
+              reviews: new Array(), // list of review _id's
          good_reviews: 0
       });
-      showfiguretoreview();
-      changeview('get-review', 'write-review');
+      Session.set('issubmitting', false);
+      Session.set('isreviewing', true);
+      scroll(0,0);
     }
   });
   
@@ -52,17 +104,7 @@ if (Meteor.isClient) {
     filepicker.setKey("AcN4KNYMSeats1v5zAAhMz");
     filepicker.constructWidget(document.getElementById('get-review-upload-fp'));
   };
-  
-  function showfiguretoreview() {
-    var fig = Figures.findOne({}, {sort : {good_reviews:1, submission_time:1}});
-    Session.set('fig_id_to_review', fig._id);
-    document.getElementById('write-review-fig-preview').src = fig.figure_url;
-  }
-  
-  function changeview(id1,id2) {
-    document.getElementById(id1).style.display = "none";
-    document.getElementById(id2).style.display = "block";
-  }
+
 }
 
 if (Meteor.isServer) {
